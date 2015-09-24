5a853d011f4d4c9d2ed8b37fc093fa150dad6895
Tim Kuminecz <tkuminecz@gmail.com>
2015-09-24 12:08:31 -0400

    update mining wrapper script

---
+++ mine.sh
@@ -0,0 +1,19 @@
+# parse arguments
+while getopts ":h:u:p:" opt; do
+	case $opt in
+		h)
+			HOST=$OPTARG
+			;;
+		u)
+			USER=$OPTARG
+			;;
+		p)
+			PASS=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			;;
+	esac
+done
+
+vagrant ssh -c "cd /vagrant/bfgminer && ./bfgminer -S zeusminer:/dev/ttyUSB0 --set-device zeusminer:ignore_golden_nonce=1 --set-device zeusminer:clock=340 --scrypt -o $HOST -u $USER -p $PASS"
